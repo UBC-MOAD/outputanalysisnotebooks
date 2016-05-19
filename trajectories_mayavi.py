b88fea5db87f8ccfe5026c1ace3eeee2d41cd1fd
Karina Ramos Musalem <kramosmu@eos.ubc.ca>
2016-05-19 09:49:13 -0700

    Made th scrippt independent of my module ReadOutTools

--- trajectories_mayavi.py
+++ trajectories_mayavi.py
@@ -2,68 +2,35 @@ from math import *
 
 from mayavi import mlab
 
-import matplotlib.colors as mcolors
-
-#from MITgcmutils import rdmds
-
 from netCDF4 import Dataset
 
 import numpy as np
 
-import os 
+# Bathymetry input
+grid =Dataset('/ocean/kramosmu/MITgcm/TracerExperiments/CNTDIFF/run29/gridGlob_cropped.nc')
 
-import pylab as pl
-
-import sys
-
-from matplotlib import pylab
-
-import matplotlib.cm as cm
+xc = grid.variables['XC'][:,:] # x coords tracer cells
+yc = grid.variables['YC'][:,:] # y coords tracer cells
+bathy = grid.variables['Depth'][:,:]
 
 
-lib_path = os.path.abspath('../../Building_canyon/BuildCanyon/PythonModulesMITgcm') # Add absolute path to my python scripts
-#lib_path = os.path.abspath('../BuildCanyon/PythonModulesMITgcm') # Add absolute path to my python scripts
 
-sys.path.append(lib_path)
+# Particle trajectories
+trajFile = Dataset('/ocean/kramosmu/Ariane/TracerExperiments/CNTDIFF/run29_10part/ariane_trajectories_qualitative.nc','r');
 
-import ReadOutTools_MITgcm as rout
-import MetricsPythonTools as mpt
+f_lont = trajFile.variables['traj_lon']
+f_latt = trajFile.variables['traj_lat']
+f_dept = trajFile.variables['traj_depth']
 
-
-#########
-CGrid ='/ocean/kramosmu/MITgcm/TracerExperiments/CNTDIFF/run29/gridGlob_cropped.nc'
-CGridOut = Dataset(CGrid )
-
-CGridOut=Dataset(CGrid)
-CState ='/ocean/kramosmu/MITgcm/TracerExperiments/CNTDIFF/run29/stateGlob_cropped.nc' 
-
-# General input
-
-xc = rout.getField(CGrid, 'XC') # x coords tracer cells
-yc = rout.getField(CGrid, 'YC') # y coords tracer cells
-
-bathy = rout.getField(CGrid,'Depth')
-
-#########
-
-f = Dataset('/ocean/kramosmu/Ariane/TracerExperiments/CNTDIFF/run29_10part/ariane_trajectories_qualitative.nc','r');
-
-f_lont=f.variables['traj_lon']
-f_latt=f.variables['traj_lat']
-f_dept=f.variables['traj_depth']
-
-
-#########
 npart = len(f_lont[1,:])
-
 partRange = np.arange(npart)
+
+
+
+# mayavi figure
 mlab.figure(size=(10, 10), bgcolor=(0.16, 0.28, 0.46))
 
 surf = mlab.mesh(xc,yc,-bathy*30.0, color=(250.0/255,235.0/255,215.0/255),scale_factor=0.1) 
-#surf.actor.actor.mapper.scalar_visibility = False
-#surf.actor.enable_texture = True
-#surf.actor.tcoord_generator_mode = 'plane'
-#mlab.show()
 
 for nn in partRange:
   trajectory  = mlab.plot3d(f_lont[:34,nn], f_latt[:34,nn], f_dept[:34,nn]*30.0, f_dept[:34,nn], 
